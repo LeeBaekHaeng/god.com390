acd076e533e764c7b609660ca31e6f2d6c17d3c9
이백행 <dlqorgod@naver.com>
2020-12-07 21:49:56 +0900

    조직정보를 코드형태로 리턴한다. 테스트

---
+++ god.test/src/test/java/egovframework/com/cmm/service/impl/CmmUseDAOTest_selectOgrnztIdDetail.java
@@ -0,0 +1,61 @@
+package egovframework.com.cmm.service.impl;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+import egovframework.com.cmm.ComDefaultCodeVO;
+import egovframework.com.cmm.service.CmmnDetailCode;
+import egovframework.com.cmm.service.EgovCmmUseService;
+import god.test.GodTestAbstract;
+
+public class CmmUseDAOTest_selectOgrnztIdDetail extends GodTestAbstract {
+
+	protected Logger egovLogger = LoggerFactory.getLogger(CmmUseDAOTest_selectOgrnztIdDetail.class);
+
+	@Autowired
+	@Qualifier("EgovCmmUseService")
+	private EgovCmmUseService egovCmmUseService;
+
+	@Override
+	@Test
+	public void test() {
+		ComDefaultCodeVO vo = new ComDefaultCodeVO();
+		vo.setTableNm("COMTNORGNZTINFO");
+		vo.setHaveDetailCondition("Y");
+		vo.setDetailCondition("ORGNZT_0000000000000");
+
+		egovLogger.debug("vo: {}", vo);
+
+		List<CmmnDetailCode> results = selectOgrnztIdDetail(vo);
+
+		egovLogger.debug("results: {}", results);
+
+		for (CmmnDetailCode result : results) {
+			egovLogger.debug("result: {}", result);
+			egovLogger.debug("getCodeId: {}", result.getCodeId());
+			egovLogger.debug("getCode: {}", result.getCode());
+			egovLogger.debug("getCodeNm: {}", result.getCodeNm());
+			egovLogger.debug("getCodeDc: {}", result.getCodeDc());
+
+			assertEquals(result.getCode(), vo.getDetailCondition());
+		}
+	}
+
+	private List<CmmnDetailCode> selectOgrnztIdDetail(ComDefaultCodeVO vo) {
+		try {
+			return egovCmmUseService.selectOgrnztIdDetail(vo);
+		} catch (Exception e) {
+			egovLogger.error(e.getMessage());
+			return new ArrayList<>();
+		}
+	}
+
+}
