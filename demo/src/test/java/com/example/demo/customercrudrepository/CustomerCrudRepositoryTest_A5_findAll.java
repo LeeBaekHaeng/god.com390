31f63cc04501d3e9c72f40cb6170e4659cbd0057
이백행 <dlqorgod@naver.com>
2020-12-22 07:55:33 +0900

    CustomerCrudRepository findAll 단위테스트

---
+++ demo/src/test/java/com/example/demo/customercrudrepository/CustomerCrudRepositoryTest_A5_findAll.java
@@ -0,0 +1,38 @@
+package com.example.demo.customercrudrepository;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.demo.Customer;
+import com.example.demo.CustomerCrudRepository;
+
+@SpringBootTest
+class CustomerCrudRepositoryTest_A5_findAll {
+
+	private static final Logger log = LoggerFactory.getLogger(CustomerCrudRepositoryTest_A5_findAll.class);
+
+	private final CustomerCrudRepository repository;
+
+	@Autowired
+	public CustomerCrudRepositoryTest_A5_findAll(CustomerCrudRepository repository) {
+		this.repository = repository;
+	}
+
+	@Test
+	void test() {
+		Iterable<Customer> customers = repository.findAll();
+
+		log.debug("customers: {}", customers);
+
+		customers.forEach(customer -> {
+			log.debug("customer: {}", customer);
+			log.debug("getId: {}", customer.getId());
+			log.debug("getFirstName: {}", customer.getFirstName());
+			log.debug("getLastName: {}", customer.getLastName());
+		});
+	}
+
+}
