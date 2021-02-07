a99d7928150819d4ac2dd24e92910dbd58478f5e
이백행 <dlqorgod@naver.com>
2021-02-08 07:26:16 +0900

    AuthorRoleManageDAO '수정된 권한 롤 관계정보를 데이터베이스에 반영' 테스트

---
+++ god.test/src/test/java/egovframework/com/sec/ram/service/impl/authorrolemanagedao/A4_updateAuthorRole.java
@@ -0,0 +1,36 @@
+package egovframework.com.sec.ram.service.impl.authorrolemanagedao;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import egovframework.com.sec.ram.service.AuthorRoleManage;
+import egovframework.com.sec.ram.service.impl.AuthorRoleManageDAO;
+import god.test.GodTestAbstract;
+
+public class A4_updateAuthorRole extends GodTestAbstract {
+
+	@Autowired
+	private AuthorRoleManageDAO dao;
+
+	@Override
+	@Test
+	public void test() {
+		egovLogger.debug("test");
+
+		AuthorRoleManage authorRoleManage = new AuthorRoleManage();
+		authorRoleManage.setAuthorCode("ROLE_ADMIN");
+		authorRoleManage.setRoleCode("web-000001");
+
+		boolean result = false;
+
+		try {
+			dao.updateAuthorRole(authorRoleManage);
+			result = true;
+		} catch (Exception e) {
+			egovLogger.error(e.getMessage());
+		}
+
+		egovLogger.debug("result: {}", result);
+	}
+
+}
