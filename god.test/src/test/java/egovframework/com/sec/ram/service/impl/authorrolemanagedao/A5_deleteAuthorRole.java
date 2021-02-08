d23c0ce9df72e1f57d8b4000df46f200d7001ed3
이백행 <dlqorgod@naver.com>
2021-02-09 06:11:39 +0900

    AuthorRoleManageDAO '권한 롤 관계정보를 화면에 조회하여 데이터베이스에서 삭제' 테스트

---
+++ god.test/src/test/java/egovframework/com/sec/ram/service/impl/authorrolemanagedao/A5_deleteAuthorRole.java
@@ -0,0 +1,36 @@
+package egovframework.com.sec.ram.service.impl.authorrolemanagedao;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import egovframework.com.sec.ram.service.AuthorRoleManage;
+import egovframework.com.sec.ram.service.impl.AuthorRoleManageDAO;
+import god.test.GodTestAbstract;
+
+public class A5_deleteAuthorRole extends GodTestAbstract {
+
+	@Autowired
+	private AuthorRoleManageDAO dao;
+
+	@Override
+	@Test
+	public void test() {
+		egovLogger.debug("test");
+
+		AuthorRoleManage authorRoleManage = new AuthorRoleManage();
+		authorRoleManage.setAuthorCode("ROLE_ADMIN");
+		authorRoleManage.setRoleCode("web-000003");
+
+		boolean result = false;
+
+		try {
+			dao.deleteAuthorRole(authorRoleManage);
+			result = true;
+		} catch (Exception e) {
+			egovLogger.error(e.getMessage());
+		}
+
+		egovLogger.debug("result: {}", result);
+	}
+
+}
