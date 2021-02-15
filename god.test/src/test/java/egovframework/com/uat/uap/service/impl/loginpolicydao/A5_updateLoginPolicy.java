bd4966d45ffa736e7bda3be1160048076df808eb
이백행 <dlqorgod@naver.com>
2021-02-16 07:59:10 +0900

    LoginPolicyDAO '기 등록된 로그인정책 정보를 수정한다.' 테스트

---
+++ god.test/src/test/java/egovframework/com/uat/uap/service/impl/loginpolicydao/A5_updateLoginPolicy.java
@@ -0,0 +1,34 @@
+package egovframework.com.uat.uap.service.impl.loginpolicydao;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import egovframework.com.uat.uap.service.LoginPolicy;
+import egovframework.com.uat.uap.service.LoginPolicyVO;
+import egovframework.com.uat.uap.service.impl.LoginPolicyDAO;
+import god.test.GodTestAbstract;
+
+public class A5_updateLoginPolicy extends GodTestAbstract {
+
+	@Autowired
+	private LoginPolicyDAO dao;
+
+	@Override
+	@Test
+	public void test() {
+		egovLogger.debug("test");
+		LoginPolicy loginPolicy = new LoginPolicyVO();
+		loginPolicy.setEmplyrId("TEST1");
+		loginPolicy.setIpInfo("test IP정보");
+		loginPolicy.setDplctPermAt("Y");
+		loginPolicy.setLmttAt("Y");
+
+		try {
+			dao.insertLoginPolicy(loginPolicy);
+			dao.updateLoginPolicy(loginPolicy);
+		} catch (Exception e) {
+			egovLogger.error(e.getMessage());
+		}
+	}
+
+}
