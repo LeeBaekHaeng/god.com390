17878d2a1f8e117231d39d7cf88dfc8f8d92858b
이백행 <dlqorgod@naver.com>
2021-02-18 07:37:31 +0900

    LoginPolicyDAO '로그인정책에 대한 현재 반영되어 있는 결과를 조회한다.' 테스트

---
+++ god.test/src/test/java/egovframework/com/uat/uap/service/impl/loginpolicydao/A7_selectLoginPolicyResult.java
@@ -0,0 +1,47 @@
+package egovframework.com.uat.uap.service.impl.loginpolicydao;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import egovframework.com.uat.uap.service.LoginPolicy;
+import egovframework.com.uat.uap.service.LoginPolicyVO;
+import egovframework.com.uat.uap.service.impl.LoginPolicyDAO;
+import god.test.GodTestAbstract;
+
+public class A7_selectLoginPolicyResult extends GodTestAbstract {
+
+	@Autowired
+	private LoginPolicyDAO dao;
+
+	@Override
+	@Before
+	public void setUp() throws Exception {
+		LoginPolicy loginPolicy = new LoginPolicy();
+		loginPolicy.setEmplyrId("TEST1");
+		loginPolicy.setIpInfo("test IP정보");
+		loginPolicy.setDplctPermAt("Y");
+		loginPolicy.setLmttAt("Y");
+
+		try {
+			dao.insertLoginPolicy(loginPolicy);
+		} catch (Exception e) {
+			egovLogger.error(e.getMessage());
+		}
+	}
+
+	@Override
+	@Test
+	public void test() {
+		egovLogger.debug("test");
+		LoginPolicyVO loginPolicyVO = new LoginPolicyVO();
+		loginPolicyVO.setEmplyrId("TEST1");
+
+		try {
+			dao.selectLoginPolicyResult(loginPolicyVO);
+		} catch (Exception e) {
+			egovLogger.error(e.getMessage());
+		}
+	}
+
+}
