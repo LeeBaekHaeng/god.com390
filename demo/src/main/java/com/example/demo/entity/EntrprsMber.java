60a5eed323545494d4532a54e95817dc95a3df43
이백행 <dlqorgod@naver.com>
2021-02-23 08:23:18 +0900

    기업회원 저장 Service

--- demo/src/main/java/com/example/demo/entity/EntrprsMber.java
+++ demo/src/main/java/com/example/demo/entity/EntrprsMber.java
@@ -17,6 +17,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.ToString;
 
 @Entity
@@ -25,7 +26,10 @@ import lombok.ToString;
 @DynamicUpdate
 
 @Getter
-//@Setter
+
+// 임시
+@Setter
+
 @ToString
 
 @NoArgsConstructor
