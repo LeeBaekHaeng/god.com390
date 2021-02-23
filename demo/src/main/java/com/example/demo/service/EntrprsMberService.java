8dc8698eb808a28fed3fc334deba153d3ee2a8aa
이백행 <dlqorgod@naver.com>
2021-02-24 06:01:42 +0900

    기업회원 페이징 Service

--- demo/src/main/java/com/example/demo/service/EntrprsMberService.java
+++ demo/src/main/java/com/example/demo/service/EntrprsMberService.java
@@ -1,8 +1,16 @@
 package com.example.demo.service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.dto.EntrprsMberDto;
+import com.example.demo.entity.EntrprsMber;
 import com.example.demo.repository.EntrprsMberRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -19,6 +27,12 @@ public class EntrprsMberService {
 	public EntrprsMberDto save(EntrprsMberDto dto) {
 		log.debug("dto: {}", dto);
 		return repository.save(dto.of()).of();
+	}
+
+	public Page<EntrprsMberDto> findAll(Specification<EntrprsMber> spec, Pageable pageable) {
+		Page<EntrprsMber> page = repository.findAll(spec, pageable);
+		List<EntrprsMberDto> content = page.get().map(mapper -> mapper.of()).collect(Collectors.toList());
+		return new PageImpl<>(content, pageable, page.getTotalElements());
 	}
 
 }
