1b6f169191466f0e9f0a195a78b7942e82358b3a
이백행 <dlqorgod@naver.com>
2021-03-23 08:11:43 +0900

    공통분류코드 Entity 생성

---
+++ demo/src/main/java/com/example/demo/entity/CmmnClCode.java
@@ -0,0 +1,59 @@
+package com.example.demo.entity;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Table(name = "COMTCCMMNCLCODE")
+
+@DynamicInsert
+@DynamicUpdate
+
+@Getter
+
+@ToString(of = { "clCode", "clCodeNm" })
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+
+@Builder
+public class CmmnClCode {
+
+	@Id
+	@Column(length = 3)
+	private String clCode;
+
+	@Column(length = 60)
+	private String clCodeNm;
+
+	@Column(length = 200)
+	private String clCodeDc;
+
+	@Column(length = 1)
+	private String useAt;
+
+	private LocalDateTime frstRegistPnttm;
+
+	@Column(length = 20)
+	private String frstRegisterId;
+
+	private LocalDateTime lastUpdtPnttm;
+
+	@Column(length = 20)
+	private String lastUpdusrId;
+
+}
