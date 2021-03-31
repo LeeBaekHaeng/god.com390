53a538e9a3758ff9e974825eb849301d6f5aaf22
이백행 <dlqorgod@naver.com>
2021-04-01 07:56:15 +0900

    '권한 롤 관계정보를 화면에 조회하여 데이터베이스에서 삭제' Service 테스트

---
+++ god.test/src/test/java/egovframework/com/sec/ram/service/impl/egovauthorrolemanageserviceimpl/A5_deleteAuthorRole.java
@@ -0,0 +1,42 @@
+package egovframework.com.sec.ram.service.impl.egovauthorrolemanageserviceimpl;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import egovframework.com.sec.ram.service.AuthorRoleManage;
+import egovframework.com.sec.ram.service.EgovAuthorRoleManageService;
+import god.test.GodTestAbstract;
+
+public class A5_deleteAuthorRole extends GodTestAbstract {
+
+	@Autowired
+	EgovAuthorRoleManageService egovAuthorRoleManageService;
+
+	@Override
+	@Test
+	public void test() {
+		egovLogger.debug("test");
+
+		// given
+		AuthorRoleManage authorRoleManage = new AuthorRoleManage();
+		authorRoleManage.setAuthorCode("ROLE_USER");
+		authorRoleManage.setRoleCode("web-000003");
+
+		// when
+		boolean result = false;
+		try {
+			egovAuthorRoleManageService.deleteAuthorRole(authorRoleManage);
+			result = true;
+		} catch (Exception e) {
+			egovLogger.error(e.getMessage());
+		}
+
+		// then
+		assertEquals(result, true);
+
+		egovLogger.debug("result={}", result);
+	}
+
+}
