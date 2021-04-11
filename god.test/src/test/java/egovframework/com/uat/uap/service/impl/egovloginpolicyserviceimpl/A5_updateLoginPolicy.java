afc4c9f4273c4607a94ae062f92ebcf8c51ef6a0
이백행 <dlqorgod@naver.com>
2021-04-12 07:13:32 +0900

    '기 등록된 로그인정책 정보를 수정한다.' Service 테스트

---
+++ god.test/src/test/java/egovframework/com/uat/uap/service/impl/egovloginpolicyserviceimpl/A5_updateLoginPolicy.java
@@ -0,0 +1,45 @@
+package egovframework.com.uat.uap.service.impl.egovloginpolicyserviceimpl;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import egovframework.com.uat.uap.service.EgovLoginPolicyService;
+import egovframework.com.uat.uap.service.LoginPolicy;
+import god.test.GodTestAbstract;
+
+//@Rollback(false)
+public class A5_updateLoginPolicy extends GodTestAbstract {
+
+	@Autowired
+	EgovLoginPolicyService service;
+
+	@Override
+	@Test
+	public void test() {
+		egovLogger.debug("test");
+
+		// given
+		LoginPolicy loginPolicy = new LoginPolicy();
+		loginPolicy.setEmplyrId("TEST1");
+		loginPolicy.setIpInfo("127.0.0.1");
+		loginPolicy.setDplctPermAt("Y");
+		loginPolicy.setLmttAt("Y");
+
+		// when
+		boolean result = false;
+		try {
+			service.updateLoginPolicy(loginPolicy);
+			result = true;
+		} catch (Exception e) {
+			egovLogger.error(e.getMessage());
+		}
+
+		// then
+		assertEquals(result, true);
+
+		egovLogger.debug("result={}", result);
+	}
+
+}
